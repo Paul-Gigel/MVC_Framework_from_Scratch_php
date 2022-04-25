1287bddaf5dd728f8fff657d86a7e753656e9bd6
Paul Gigel <91072840+paul5848-Dresden@users.noreply.github.com>
2022-04-25 15:38:34 +0200

    25.04.2022 15:38

--- core/Router.php
+++ core/Router.php
@@ -3,6 +3,7 @@ namespace app\core;
 class Router
 {
     public Request $request;
+    public Response $response;
     protected array $routes = [
        /*
         * 'get' => [
@@ -18,9 +19,10 @@ class Router
     /**
      * @param Request $request
      */
-    public function __construct(Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback)
@@ -34,18 +36,34 @@ class Router
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
         if ($callback === false)    {
+            $this->response->setStatusCode(404);
             return "not found";
-            exit;
         }
         if (is_string($callback))   {
             return $this->renderView($callback);
         }
         return call_user_func($callback);
-        //var_dump($this->routes);
     }
 
     public function renderView($view)   //$view = $callback
     {
-        include_once __DIR__."/../views/$view.php";
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+        //include_once Application::$ROOT_DIR."/views/$view.php";
+    }
+
+    protected function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
     }
 }
