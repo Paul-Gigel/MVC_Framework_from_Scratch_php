f9b9ee661cf39470b178619ece17a59903e427aa
Paul Gigel <91072840+paul5848-Dresden@users.noreply.github.com>
2022-04-27 10:00:08 +0200

    27.04.2022 10:00

--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -2,5 +2,9 @@
 namespace app\models;
 class RegisterModel
 {
-    
+    public string $firstname;
+    public string $lastname;
+    public string $email;
+    public string $password;
+    public string $passwordConfirm;
 }
