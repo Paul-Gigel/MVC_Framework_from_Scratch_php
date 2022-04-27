b401f0f1d7ad447576e0aa84079ec1a0a147d4eb
Paul Gigel <91072840+paul5848-Dresden@users.noreply.github.com>
2022-04-27 10:41:41 +0200

    27.04.2022 10:41

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Request;
+use app\models\RegisterModel;
 
 class AuthController extends Controller
 {
@@ -12,10 +13,20 @@ class AuthController extends Controller
         return $this->render('login');
     }
     public function register(Request $request)  {
+        $registerModel = new RegisterModel();
         if ($request->isPost()) {
-            $register = new RegisterModel();
-            return 'Handle submitted data';
+            $registerModel->loadData($request->getBody());
+            var_dump($registerModel);
+            if($registerModel->validate() && $registerModel->register())    {
+                return 'Success';
+            }
+            return $this->render("register",[
+                'model' => $registerModel
+            ]);
         }
+        return $this->render("register",[
+            'model' => $registerModel
+        ]);
         $this->setLayout('auth');
         return $this->render('register');
     }
