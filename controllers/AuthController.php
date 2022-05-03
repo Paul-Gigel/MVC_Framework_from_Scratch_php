524e59c1227c7300ea8805cf5034be80a8358fc1
Paul Gigel <91072840+paul5848-Dresden@users.noreply.github.com>
2022-05-03 14:44:25 +0200

    03.05.2022 14:44

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Request;
-use app\models\RegisterModel;
+use app\models\User;
 
 class AuthController extends Controller
 {
@@ -13,7 +13,7 @@ class AuthController extends Controller
         return $this->render('login');
     }
     public function register(Request $request)  {
-        $registerModel = new RegisterModel();
+        $registerModel = new User();
         if ($request->isPost()) {
             $registerModel->loadData($request->getBody());
              //var_dump($registerModel);
--- models/User.php
+++ models/User.php
@@ -1,18 +1,23 @@
 <?php
 namespace app\models;
+use app\core\DbModel;
 use app\core\Model;
 
-class RegisterModel extends Model
+class User extends DbModel
 {
     public string $firstname = '';
     public string $lastname = '';
     public string $email = '';
     public string $password = '';
     public string $confirmPassword = '';
+    public function tableName(): string
+    {
+        return 'users';
+    }
+
     public function register()
     {
-        echo "creating new user";
-        return true;
+        return $this->save();
     }
     public function rules(): array
     {
@@ -23,5 +28,9 @@ class RegisterModel extends Model
             'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 24]],
             'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH,'match' => 'password']]
         ];
+    }
+    public function attributes(): array
+    {
+        return ['firstname', 'lastname', 'email', 'password'];
     }
 }
